7f9b81ab5d28d3b0023d47e479bc0712d9e862e6
Juan Londono <juan.londono@uq.edu.au>
2020-06-19 15:58:00 +1000

    Add timer and ending message (Game Over or Nice)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// STAR MATCH - V6
+// STAR MATCH - V7
 
 const StarsDisplay = (props) => (
   <>
@@ -22,6 +22,12 @@ const PlayNumber = (props) => (
 
 const PlayAgain = (props) => (
   <div className="game-done">
+    <div
+      className="message"
+      style={{ color: props.gameStatus === 'lost' ? 'red' : 'green' }}
+    >
+      {props.gameStatus === 'lost' ? 'Game Over' : 'Nice'}
+    </div>
     <button onClick={props.onClick}>Play Again</button>
   </div>
 );
@@ -31,8 +37,21 @@ const StarMatch = () => {
   const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
   const [candidateNums, setCandidateNums] = useState([]);
 
+  const [secondsLeft, setSecondsLeft] = useState(10);
+
+  useEffect(() => {
+    if (secondsLeft > 0 && availableNums.length > 0) {
+      const timerId = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    }
+  });
+
   const candidatesAreWrong = utils.sum(candidateNums) > stars;
-  const gameIsDone = availableNums.length === 0;
+
+  const gameStatus =
+    availableNums.length === 0 ? 'won' : secondsLeft === 0 ? 'lost' : 'active';
 
   const resetGame = () => {
     setStars(utils.random(1, 9));
@@ -51,7 +70,7 @@ const StarMatch = () => {
   };
 
   const onNumberClick = (number, currentStatus) => {
-    if (currentStatus === 'used') {
+    if (gameStatus !== 'active' || currentStatus === 'used') {
       return;
     }
 
@@ -79,8 +98,8 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          {gameIsDone ? (
-            <PlayAgain onClick={resetGame} />
+          {gameStatus !== 'active' ? (
+            <PlayAgain onClick={resetGame} gameStatus={gameStatus} />
           ) : (
             <StarsDisplay count={stars} />
           )}
@@ -96,7 +115,7 @@ const StarMatch = () => {
           ))}
         </div>
       </div>
-      <div className="timer">Time Remaining: 10</div>
+      <div className="timer">Time Remaining: {secondsLeft}</div>
     </div>
   );
 };
